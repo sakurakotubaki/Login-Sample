5a7fabbe20df1b6614e9a600bd72122b314f1549
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-05-15 12:27:36 +0900

    Login UI OK

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.teal,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ログイン画面"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text("ToDoアプリ", style: TextStyle(fontSize: 50),),
+                  Text("ログインしてください。"),
+                ],
+              ),
+          ),
+          CustomTextField(label: "メールアドレス"),
+          CustomTextField(label: "パスワード"),
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text("新規登録は"), TextButton(onPressed: () {}, child: Text("こちら"),)],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  String label;
+
+  CustomTextField({
+    required this.label,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          onChanged: (newText){},
+          decoration: InputDecoration(
+            labelText: label,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16))
+            )
+          ),
+        ),
+    );
+  }
+}
